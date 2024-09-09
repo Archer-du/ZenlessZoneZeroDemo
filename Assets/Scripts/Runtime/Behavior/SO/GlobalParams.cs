7761685d784fc07a8a786b4a0cc5cf69d509f697
Archer_du <dupengche@gmail.com>
2024-09-09 20:18:08 +0800

    sync: building basic config layer

---
+++ Assets/Scripts/Runtime/Behavior/SO/GlobalParams.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ZZZDemo.Runtime.Behavior.SO
+{
+    [CreateAssetMenu(fileName = "GlobalParams", menuName = "ZZZDemo/Config/GlobalParam", order = 0)]
+    public class GlobalParams : ScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Runtime/Model/Config/GlobalConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace ZZZDemo.Runtime.Model.Config
+{
+    public static class GlobalConstants
+    {
+        
+    }
+}
